29e29727ede35146b8ab948bb919dd4447036ff9
Alexander Barke <Alexander.Barke@sea.live>
2023-06-07 17:18:25 +0800

    eslint

--- init.lua
+++ init.lua
@@ -96,6 +96,12 @@ require('lazy').setup({
     end,
   },
 
+
+  -- eslint related plugins
+  { 'neovim/nvim-lspconfig' },
+  { 'jose-elias-alvarez/null-ls.nvim' },
+  { 'MunifTanjim/eslint.nvim' },
+
   -- Editor tabs
   {'romgrk/barbar.nvim',
     dependencies = {
@@ -337,6 +343,40 @@ require('telescope').setup {
     }
   },
 }
+
+local eslint = require("eslint")
+local status, null_ls = pcall(require, "null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+    null_ls.builtins.diagnostics.fish
+  }
+})
+
+
+eslint.setup({
+  bin = 'eslint_d', -- or `eslint_d`
+  code_actions = {
+    enable = true,
+    apply_on_save = {
+      enable = true,
+      types = { "directive", "problem", "suggestion", "layout" },
+    },
+    disable_rule_comment = {
+      enable = true,
+      location = "separate_line", -- or `same_line`
+    },
+  },
+  diagnostics = {
+    enable = true,
+    report_unused_disable_directives = false,
+    run_on = "type", -- or `save`
+  },
+})
+
 -- open telescope when neovim starts only if no other plugin is opening
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
